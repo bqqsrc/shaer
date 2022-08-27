dd3d305b3796e535a229ba251317b3f02220d007
WangXiaojie01 <1259488569@qq.com>
2022-08-27 15:49:54 +0800

    提交代码

--- shaer.go
+++ shaer.go
@@ -1,1 +1,21 @@
 package shaer
+
+import (
+	"crypto/sha1"
+	"fmt"
+)
+
+func StrSHA1(word string, salts ...string) string {
+	bytes := []byte(word)
+	for _, salt := range salts {
+		bytes = BytesSHA1(bytes, salt)
+	}
+	return fmt.Sprintf("%x", bytes)
+}
+
+func BytesSHA1(word []byte, salts string) []byte {
+	s := []byte(salts)
+	p := append(word, s...)
+	h := sha1.Sum(p)
+	return h[:]
+}
